02af51ce6f1d89581b8a1ba74066aa5baa65c0ad
Andrés Felipe Arango <andresarangopro@gmail.com>
2019-09-29 22:24:27 -0500

    server

---
+++ runserver.py
@@ -0,0 +1,8 @@
+import os 
+#os.system("cd C:\\Users\\FELIPE\\Documents\\app_server\\env\\Scripts ")
+#os.system("cd C:\Users\FELIPE\Documents\app_server\env\Scripts")
+os.system("start C:\\Users\\FELIPE\\Documents\\app_server\\env\\Scripts\\activate /wait cmd /c'ipconfig'")
+
+#os.system("cd env/Scripts /start activate /wait cmd/c{pause}")
+#os.system("start cmd /wait cmd /c'pause' / PAUSE")
+
